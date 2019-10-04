1c1b01a6cafc60f049b1f598c77f36bd758ff5e9
Mark Canlas <github@htmlism.com>
2019-10-04 12:39:11 -0400

    add raison d'etre

--- src/main/scala/com/htmlism/GenerateReadme.scala
+++ src/main/scala/com/htmlism/GenerateReadme.scala
@@ -3,6 +3,9 @@ package com.htmlism
 import cats.effect._
 import cats.implicits._
 
+/**
+ * Regenerates the `README.md` file based on the manuscript so that the GitHub view is synchronized with the book.
+ */
 object GenerateReadme extends IOApp {
   def run(args: List[String]): IO[ExitCode] =
     for {
