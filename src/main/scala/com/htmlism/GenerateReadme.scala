a626b1d303bb962d3468b9e9fe68ac351021a300
Mark Canlas <github@htmlism.com>
2019-10-17 17:46:56 -0400

    factor out f

--- src/main/scala/com/htmlism/GenerateReadme.scala
+++ src/main/scala/com/htmlism/GenerateReadme.scala
@@ -8,9 +8,12 @@ import cats.implicits._
  */
 object GenerateReadme extends IOApp {
   def run(args: List[String]): IO[ExitCode] =
+    program[IO]
+
+  def program[F[_]: Sync]: F[ExitCode] =
     for {
-      book <- BookReaderAlg[IO].pure[IO]
-      readme <- ReadmeWriterAlg[IO].pure[IO]
+      book <- BookReaderAlg[F].pure[F]
+      readme <- ReadmeWriterAlg[F].pure[F]
 
       toc <- book.readChapterHeadings
       _ <- readme.write(toc)
