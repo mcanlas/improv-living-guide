19bd15d606d685aa8afa5f5be35cc5dea55740c3
Mark Canlas <github@htmlism.com>
2019-10-21 17:31:31 -0400

    use fp combinators

--- src/main/scala/com/htmlism/ReadmeWriterAlg.scala
+++ src/main/scala/com/htmlism/ReadmeWriterAlg.scala
@@ -14,23 +14,20 @@ object ReadmeWriterAlg {
   def apply[F[_]](reader: ReaderAlg[F], writer: WriterAlg[F])(implicit F: Sync[F]): ReadmeWriterAlg[F] =
     new ReadmeWriterAlg[F] {
       def write(toc: List[(String, String)]): F[Unit] =
-        (for {
-          readme <- reader.lines(file)
-        } yield {
-          val newToc =
-            toc.map((format _).tupled)
-
-          val newReadmeLines =
-            keepUpToChapter(readme)._1 ::: newToc
-
-          val payload =
-            (newReadmeLines :+ "")
-              .mkString("\n")
-
-          writer
-            .write(file)(payload)
-        }).void
+        reader.lines(file) >>=
+          (writer.write(file) _).compose(toPayload(toc))
     }
+
+  private def toPayload(toc: List[(String, String)])(readme: List[String]) = {
+    val newToc =
+      toc.map((format _).tupled)
+
+    val newReadmeLines =
+      keepUpToChapter(readme)._1 ::: newToc
+
+    (newReadmeLines :+ "")
+      .mkString("\n")
+  }
 
   private def format(title: String, file: String): String =
     s"* [$title](manuscript/$file)"
